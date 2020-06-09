6eb1dcb02cd8c9d829f1dcd3c468f775639df5b8
shiori-yamagishi <66342466+shiori-yamagishi@users.noreply.github.com>
2020-06-09 12:03:44 +0900

    Delete UpdateService.java

--- PHONEBOOK_springboot/src/main/java/com/ojtproject/phonebook/service/UpdateService.java
+++
@@ -1,9 +0,0 @@
-//package com.ojtproject.phonebook.service;
-
-//public class UpdateService {
-
-	//public void update(PhoneBook phoneBook) {
-      //  phoneBookRepository.save(phoneBook);
-    //}
-
-//}
